fde5aaa4499998a56cea5a9c8e4b46944ab0212e
Noahsch19 <nscheuerman19@gmail.com>
2018-09-15 18:26:42 -0700

    I initialized the repo and added initial code for my main cpp

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // introduce the game
+    constexpr int WORD_LENGTH = 5;
+    cout << "Welcome to Bulls and Cows, a fun word game!\n";
+    cout << "Can you guess the " << WORD_LENGTH;
+    cout << " letter isogram I'm thinking of?\n";
+    
+    //get a guess from the player
+    cout << "Enter your guess: ";
+    string Guess = "";
+    getline(cin, Guess);
+    //repeat the guess back to them
+    cout << "Your guess was: " << Guess << endl;
+    //get a guess from the player
+    cout << "Enter your guess: ";
+    getline(cin, Guess);
+    //repeat the guess back to them
+    cout << "Your guess was: " << Guess << endl;
+    
+    cout << endl;
+    
+    return 0;
+}
+
+
+
